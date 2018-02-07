3358dcbdadb2b76fa31676df9ea2c55bd5340b96
Nikhil Kamineni <nikhil@nk-mbp.local>
2018-02-07 17:18:50 -0500

    Populated dummy-data

---
+++ src/dummy-data.js
@@ -0,0 +1,40 @@
+const dummyData = [
+  {
+    username: 'Zephyr',
+    imageUrl: 'https://s3.amazonaws.com/filestore.rescuegroups.org/5880/pictures/animals/12201/12201353/53939364_500x500.jpg',
+    jobTitle: 'Junior Mousecatcher at The Whiskers Foundation',
+    connections: 'Pickle and 339 others'
+  },
+  {
+    username: 'Pickle',
+    imageUrl: 'https://iheartcats.com/wp-content/uploads/2015/12/shutterstock_171850070.jpg',
+    jobTitle: 'Senior Napper at Catnip Industries',
+    connections: 'Zephyr and 272 others'
+  },
+  {
+    username: 'Emma',
+    imageUrl: 'https://www.catsvermont.com/img/1876/903.jpg',
+    jobTitle: 'Junior Mousecatcher at Mousesanto',
+    connections: 'Moose and 276 others'
+  },
+  {
+    username: 'Merlin',
+    imageUrl: 'https://s3.amazonaws.com/filestore.rescuegroups.org/5880/pictures/animals/12456/12456321/53870787_500x500.jpg',
+    jobTitle: 'Senior Product Manager at Hairball and Associates',
+    connections: 'Biscuit and 783 others'
+  },
+  {
+    username: 'Moose',
+    imageUrl: 'https://static.postize.com/images/HClfXMrupiIc_762.jpg',
+    jobTitle: 'Chief Muffin Maker at Kibbles.com',
+    connections: 'Merlin and 123 others'
+  },
+  {
+    username: 'Biscuit',
+    imageUrl: 'https://iheartcats.com/wp-content/uploads/2015/12/shutterstock_171850070.jpg',
+    jobTitle: 'Head Dreamer at Catbeds Inc.',
+    connections: 'Pickle and 339 others'
+  },
+];
+
+export default dummData;
