c70673bde0f12aed5b8f29e50646b399e8598d81
Nikhil Kamineni <nikhil@nk-mbp.local>
2018-02-07 23:12:53 -0500

    Populated connections and added styling

--- src/components/connections/connections.js
+++ src/components/connections/connections.js
@@ -3,18 +3,56 @@ import styled from 'styled-components';
 
 function Connections() {
   const ConnectionsStyles = styled.div`
-    width: 280px;
+    width: 250px;
     height: 400px;
-    border: 1px solid #CACCCE;
     padding: 0;
     margin: 0 5px;
+    display: flex;
+    flex-direction: column;
+
+    h1, h3 {
+      color: #0077B5;
+    }
+
+    .connectionsNumber {
+      width: 250px;
+      height: 200px;
+      border: 1px solid #CACCCE;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+    }
+
+    .addContact {
+      width: 250px;
+      border: 1px solid #CACCCE;
+      border-top: 0px;
+    }
+
+    .learnMore {
+      width: 250px;
+      border: 1px solid #CACCCE;
+      border-top: 0px;
+    }
+
   `;
 
   return (
     <ConnectionsStyles className='connections'>
-      <h1>534</h1>
-      <h2>Your connections</h2>
-      <h3>See all</h3>
+      <div className='connectionsNumber'>
+        <h1>534</h1>
+        <h2>Your connections</h2>
+        <h3>See all</h3>
+      </div>
+      <div className='addContact'>
+        <h2>Add person contact</h2>
+        <form><input type='text'/></form>
+        <button>Continue</button>
+        <h3>More options</h3>
+      </div>
+      <div className='learnMore'>
+      <p>We'll import your address book to suggest connections. Learn more</p>
+      </div>
     </ConnectionsStyles>
   )
 }
